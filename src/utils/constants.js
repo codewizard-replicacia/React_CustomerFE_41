87187e24c270ea11ba2a384b50f2e5a8c84b9e03
Administrator <Administrator@172.31.36.201>
2023-12-04 15:02:39 +0000

    initial commit

---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+export const BASE_URL = "http://3.133.64.67:8202/CustomerBEApp"; //Temporarily using field; Enter Backend URL here e.g. http://3.133.64.67:8164/generated_app
+export const PATH_CUSTOMER = "/Customers";
---
+++ src/utils/display_configuration.js
@@ -0,0 +1,34 @@
+export const customerViewConfig = {
+Details: [
+    {
+      key: "CustId",
+      value: "CustId",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "Quantity",
+      value: "Quantity",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "CustAccountNumber",
+      value: "CustAccountNumber",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "ItemId",
+      value: "ItemId",
+      type: "text",
+      editable: false,
+      required: true
+    },
+  ],
+Type: [
+  ],
+}
